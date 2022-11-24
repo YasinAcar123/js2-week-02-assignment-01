bfd3cc33542b9057bf9b8cb80b99849f81e3240a
YasinAcar123 <yasin.acar@hicoders.ch>
2022-11-24 17:47:42 +0100

    aa

--- functions.js
+++ functions.js
@@ -1,28 +1,22 @@
-const findTheNumberOfOddNumbers = () => {
-  const odds = findTheNumberOfOddNumbers.filter((num) => num % 2 === 1);
-  let result= odds.length
+const findTheNumberOfOddNumbers = (pArr) => {
+    let newArr= pArr.filter((item)=> item %2===1)
+    let result=newArr.length
+    return result
+  
+};
+
+const getIndexOfFalseItem = (pArr) => {
+  let result=pArr.indexOf(false)
   return result
-  console.log(result)
 };
 
-const getIndexOfFalseItem = () => {
-  var results = [];
-    var idx = getIndexOfFalseItem.indexOf(false);
-    while (idx != -1) {
-        results.push(idx);
-        idx = getIndexOfFalseItem.indexOf(false, idx + 1);
-    }
-    return results;
-};
-
-const filterNumbers = () => {
-  let fnumbers=filterNumbers.filter(pNum)
-  return fnumbers[0]
-  console.log(fnumbers[0])
+const filterNumbers = (pArr) => {
+  let result=pArr.filter((item)=> typeof item==="number")
+  return result
 };
 
 const generateEmailAddress = () => {
-let email="";
+/*let email="";
 
 for (let index = 0; index < generateEmailAddress.length; index++) {
    counter++
@@ -36,7 +30,7 @@ for (let index = 0; index < generateEmailAddress.length; index++) {
    }
 
  }
-console.log(emails)
+console.log(emails)*/
 };
 
 export {
